0948a1dc5182191525a8ee8c5839c4cb014976d2
Yaroslav <36567300+epfly6@users.noreply.github.com>
2023-07-31 11:34:50 +0300

    Postive Algiz effect no longer triggers on room revisit

--- rune_rooms_scripts/rune_effects/positive/algiz.lua
+++ rune_rooms_scripts/rune_effects/positive/algiz.lua
@@ -3,6 +3,7 @@ local PositiveAlgiz = {}
 
 function PositiveAlgiz:OnNewRoom()
     if not RuneRooms:IsPositiveEffectActive(RuneRooms.Enums.RuneEffect.ALGIZ) then return end
+    if not Game():GetRoom():IsFirstVisit() then return end
 
     local players = TSIL.Players.GetPlayers()
 
